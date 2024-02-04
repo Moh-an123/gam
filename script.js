c6e1a6b6ae377eb89c52c8a9a02894ff393a7d22
Your Name <you@example.com>
2024-02-04 12:43:08 +0530

    latest

--- script.js
+++ script.js
@@ -5,19 +5,28 @@ let c1=document.getElementById('res');
 console.log(a.length);
 let c=0,d,e=1;
 
-b.addEventListener('click',()=>{
+function  clr(){
     d=1;
-    console.log(d);
+    // console.log(d);
+    c1.innerHTML='';
     setTimeout(()=>{  for(let i=0;i<9;i++){
         a[i].innerHTML='';
     }
     c=0; i=0; e=1;  
-    c1.innerHTML='';
+  
 },250);
+document.getElementById('g').style.display='none';
+b1.style.display='block';
+b.style.display='none'
+}
+b.addEventListener('click',()=>{
+    clr();
 });
 
 b1.addEventListener('click',()=>{
-
+document.getElementById('g').style.display='grid';
+b1.style.display='none';
+b.style.display='block'
   for(let i=0;i<9&&e;i++){
   if(e==1){
   a[i].addEventListener('click',()=>{
@@ -40,37 +49,37 @@ b1.addEventListener('click',()=>{
 if(c>=5){
 
     if(a[0].innerHTML==a[1].innerHTML&&a[1].innerHTML==a[2].innerHTML){
-    if(a[0].innerHTML=='X') { c1.innerHTML='User 1 Wins'; e=0; }
-    if(a[0].innerHTML=='O') { c1.innerHTML='User 2 Wins'; e=0; }
+    if(a[0].innerHTML=='X') { c1.innerHTML='User 1 Wins'; e=0;   setTimeout(()=>{clr()},1000);}
+    if(a[0].innerHTML=='O') { c1.innerHTML='User 2 Wins'; e=0;  setTimeout(()=>{clr()},1000);}
    
     }
    else if(a[3].innerHTML==a[4].innerHTML&&a[4].innerHTML==a[5].innerHTML){
-        if(a[3].innerHTML=='X'){c1.innerHTML='User 1 Wins'; e=0;}
-        if(a[3].innerHTML=='O'){c1.innerHTML='User 2 Wins'; e=0;}
+        if(a[3].innerHTML=='X'){c1.innerHTML='User 1 Wins'; e=0; setTimeout(()=>{clr()},1000);}
+        if(a[3].innerHTML=='O'){c1.innerHTML='User 2 Wins'; e=0; setTimeout(()=>{clr()},1000);}
     }
    else if(a[6].innerHTML==a[7].innerHTML&&a[7].innerHTML==a[8].innerHTML){
-        if(a[6].innerHTML=='X'){c1.innerHTML='User 1 Wins'; e=0;}
-        if(a[6].innerHTML=='O'){c1.innerHTML='User 2 Wins'; e=0;}
+        if(a[6].innerHTML=='X'){c1.innerHTML='User 1 Wins'; e=0; setTimeout(()=>{clr()},1000);}
+        if(a[6].innerHTML=='O'){c1.innerHTML='User 2 Wins'; e=0; setTimeout(()=>{clr()},1000);}
     }
     else if(a[0].innerHTML==a[3].innerHTML&&a[3].innerHTML==a[6].innerHTML){
-        if(a[0].innerHTML=='X'){c1.innerHTML='User 1 Wins'; e=0;}
-        if(a[0].innerHTML=='O'){c1.innerHTML='User 2 Wins'; e=0;}
+        if(a[0].innerHTML=='X'){c1.innerHTML='User 1 Wins'; e=0; setTimeout(()=>{clr()},1000);}
+        if(a[0].innerHTML=='O'){c1.innerHTML='User 2 Wins'; e=0; setTimeout(()=>{clr()},1000);}
     }
     else if(a[1].innerHTML==a[4].innerHTML&&a[4].innerHTML==a[7].innerHTML){
-        if(a[1].innerHTML=='X'){c1.innerHTML='User 1 Wins';e=0;} 
-        if(a[1].innerHTML=='O'){c1.innerHTML='User 2 Wins';e=0;}
+        if(a[1].innerHTML=='X'){c1.innerHTML='User 1 Wins';e=0; setTimeout(()=>{clr()},1000);}
+        if(a[1].innerHTML=='O'){c1.innerHTML='User 2 Wins';e=0; setTimeout(()=>{clr()},1000);}
     }
     else if(a[2].innerHTML==a[5].innerHTML&&a[5].innerHTML==a[8].innerHTML){
-        if(a[2].innerHTML=='X') {c1.innerHTML='User 1 Wins'; e=0;}
-        if(a[2].innerHTML=='O') {c1.innerHTML='User 2 Wins'; e=0;}
+        if(a[2].innerHTML=='X') {c1.innerHTML='User 1 Wins'; e=0; setTimeout(()=>{clr()},1000);}
+        if(a[2].innerHTML=='O') {c1.innerHTML='User 2 Wins'; e=0; setTimeout(()=>{clr()},1000);}
     }
     else if(a[0].innerHTML==a[4].innerHTML&&a[4].innerHTML==a[8].innerHTML){
-        if(a[0].innerHTML=='X'){c1.innerHTML='User 1 Wins'; e=0;}
-        if(a[0].innerHTML=='O'){c1.innerHTML='User 2 Wins'; e=0;}
+        if(a[0].innerHTML=='X'){c1.innerHTML='User 1 Wins'; e=0; setTimeout(()=>{clr()},1000);}
+        if(a[0].innerHTML=='O'){c1.innerHTML='User 2 Wins'; e=0; setTimeout(()=>{clr()},1000);}
     }
     else if(a[2].innerHTML==a[4].innerHTML&&a[4].innerHTML==a[6].innerHTML){
-        if(a[2].innerHTML=='X'){c1.innerHTML='User 1 Wins'; e=0;}
-        if(a[2].innerHTML=='O'){c1.innerHTML='User 2 Wins'; e=0;}
+        if(a[2].innerHTML=='X'){c1.innerHTML='User 1 Wins'; e=0; setTimeout(()=>{clr()},1000);}
+        if(a[2].innerHTML=='O'){c1.innerHTML='User 2 Wins'; e=0; setTimeout(()=>{clr()},1000);}
     }
     } });
 }
